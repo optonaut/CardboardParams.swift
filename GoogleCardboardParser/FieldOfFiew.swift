ccd0eb093b0f08657cce0f25367b4e31283d3dbc
emiswelt <emiswelt@gmail.com>
2015-10-26 10:25:42 +0000

    Made FOV public.

--- GoogleCardboardParser/FieldOfFiew.swift
+++ GoogleCardboardParser/FieldOfFiew.swift
@@ -10,12 +10,20 @@ import Foundation
 
 public struct FieldOfView {
     
-    let angles: [Float]
+    public let angles: [Float]
+    public let left: Float
+    public let right: Float
+    public let top: Float
+    public let bottom: Float
     
     public init(angles: [Float]) {
         guard angles.count == 4 else {
             fatalError("FieldOfView expects four angles.")
         }
         self.angles = angles
+        self.left = angles[0]
+        self.right = angles[1]
+        self.top = angles[2]
+        self.bottom = angles[3]
     }
 }
